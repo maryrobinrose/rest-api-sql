c86b4eff3ec36ee6eecb83fd83e46f63ae4d271b
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-01 08:27:30 -0700

    configure sql

--- app.js
+++ app.js
@@ -3,6 +3,13 @@
 // load modules
 const express = require('express');
 const morgan = require('morgan');
+const Sequelize = require('sequelize');
+
+// construct the database
+const db = new Sequelize({
+  dialect: "sqlite",
+  storage: "./fsjstd-restapi.db"
+});
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
