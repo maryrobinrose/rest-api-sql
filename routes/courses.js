bfbe5bbcf64dd11de11f932793180ce82c751a05
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-01 19:11:38 -0700

    basic-auth

--- routes/courses.js
+++ routes/courses.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Course = require('../models').Course;
 const User = require('../models').User;
 const Sequelize = require('sequelize');
-const authenticate = require('./login');
+const authenticate = require('basic-auth');
 
 /* GET course list. */
 router.get('/', (req, res) => {
