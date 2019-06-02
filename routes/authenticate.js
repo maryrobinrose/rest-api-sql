9208053ee5d447f9a73378820376171420f48e24
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-02 13:36:30 -0700

    add nodes from docs

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -2,3 +2,27 @@ const User = require("../models").User;
 const Sequelize = require('sequelize');
 const authenticate = require('basic-auth');
 const bcryptjs = require('bcryptjs');
+
+
+//from https://www.npmjs.com/package/basic-auth
+// Check credentials
+  // The "check" function will typically be against your user store
+  if (!credentials || !check(credentials.name, credentials.pass)) {
+    res.statusCode = 401
+    res.setHeader('WWW-Authenticate', 'Basic realm="example"')
+    res.end('Access denied')
+  } else {
+    res.end('Access granted')
+  }
+})
+
+// Basic function to validate credentials for example
+function check (name, pass) {
+  var valid = true
+
+  // Simple method to prevent short-circut and use timing-safe compare
+  valid = compare(name, 'john') && valid
+  valid = compare(pass, 'secret') && valid
+
+  return valid
+}
