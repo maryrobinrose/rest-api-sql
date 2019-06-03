56ec2dc1985b1bfd73fab4c915759e582a5a399f
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-02 21:24:44 -0700

    uncomment routes

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ app.use(morgan('dev'));
 
 // TODO setup your api routes here
 app.use('/api', require('./routes/index'));
-//app.use('/api/users', require('./routes/user'));
-//app.use('/api/courses', require('./routes/courses'));
+app.use('/api/users', require('./routes/user'));
+app.use('/api/courses', require('./routes/courses'));
 
 
 // setup a friendly greeting for the root route
