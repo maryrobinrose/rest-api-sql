08ae134dde271bf3793133dd99dd912c7f9e3714
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-03 18:23:03 -0700

    change auth require

--- routes/courses.js
+++ routes/courses.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Course = require('../models').Course;
 const User = require('../models').User;
 const Sequelize = require('sequelize');
-const authenticate = require('basic-auth');
+const authenticate = require('./authenticate');
 const bcryptjs = require('bcryptjs');
 
 /* GET course list. */
