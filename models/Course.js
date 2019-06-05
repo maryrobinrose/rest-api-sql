ad082e878d887eb5b6ad35f25eec6ad3f519a622
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-05 12:11:50 -0700

    fix typo

--- models/Course.js
+++ models/Course.js
@@ -25,7 +25,7 @@ module.exports = function(sequelize, DataTypes) {
         }
       }
     },
-    esimatedTime: {
+    estimatedTime: {
       type: DataTypes.STRING,
       allowNull: true,
     },
